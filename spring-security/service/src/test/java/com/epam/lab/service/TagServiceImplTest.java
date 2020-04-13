4f58197a4bd3d3327f309597e29bb1c58ee51d25
Andrei_Zdanovich <Andrei_Zdanovich@epam.com>
2020-04-13 09:55:36 +0300

    add service test for tag

--- spring-security/service/src/test/java/com/epam/lab/service/TagServiceImplTest.java
+++ spring-security/service/src/test/java/com/epam/lab/service/TagServiceImplTest.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(JUnit4.class)
-@Ignore
 public class TagServiceImplTest {
     private TagService tagService;
     private TagRepository tagRepositoryMock;
@@ -34,83 +33,71 @@ public class TagServiceImplTest {
         tagService = new TagServiceImpl(tagRepositoryMock);
     }
 
-//    @Test
-//    public void shouldCreateTagDto() {
-//        TagDto tagDto = getTestTagDto();
-//        Tag tagEntity = TestUtil.convertToEntity(tagDto);
-//        tagEntity.setId(nextLong(1, Long.MAX_VALUE));
-//
-//        when(modelMapperMock.map(tagDto, Tag.class)).thenReturn(tagEntity);
-//        when(tagRepositoryMock.save(any())).thenReturn(tagEntity);
-//
-//        tagService.create(tagDto);
-//
-//        ArgumentCaptor<Tag> argumentCaptor = ArgumentCaptor.forClass(Tag.class);
-//        verify(tagRepositoryMock, times(1)).save(argumentCaptor.capture());
-//        verifyNoMoreInteractions(tagRepositoryMock);
-//
-//        Tag tag = argumentCaptor.getValue();
-//
-//        assertTrue(tag.getId() > 0);
-//        assertThat(tag.getName(), is(tagDto.getName()));
-//    }
-//
-//    @Test
-//    public void shouldReadTagDto() {
-//        TagDto tagDto = getTestTagDto();
-//        tagDto.setId(nextLong(1, Long.MAX_VALUE));
-//        Tag tagEntity = convertToEntity(tagDto);
-//
-//        when(modelMapperMock.map(tagDto, Tag.class)).thenReturn(tagEntity);
-//        when(modelMapperMock.map(tagEntity, TagDto.class)).thenReturn(tagDto);
-//        when(tagRepositoryMock.findById(anyLong())).thenReturn(Optional.of(tagEntity));
-//
-//        Optional<TagDto> dtoOptional = tagService.read(tagDto);
-//
-//        verify(tagRepositoryMock, times(1)).findById(anyLong());
-//        verifyNoMoreInteractions(tagRepositoryMock);
-//
-//        assertTrue(dtoOptional.isPresent());
-//        assertEquals(tagDto, dtoOptional.get());
-//    }
-//
-//    @Test
-//    public void shouldUpdateTagName() {
-//        TagDto tagDto = getTestTagDto();
-//        tagDto.setId(nextLong(1, Long.MAX_VALUE));
-//        Tag tagEntity = convertToEntity(tagDto);
-//        tagEntity.setName(RandomStringUtils.random(10, true, false));
-//
-//        when(modelMapperMock.map(tagDto, Tag.class)).thenReturn(tagEntity);
-//        when(modelMapperMock.map(tagEntity, TagDto.class)).thenReturn(tagDto);
-//        when(tagRepositoryMock.update(tagEntity)).thenReturn(tagEntity);
-//        when(tagRepositoryMock.findById(tagDto.getId())).thenReturn(Optional.of(tagEntity));
-//
-//        tagService.update(tagDto);
-//
-//        ArgumentCaptor<Tag> argumentCaptor = ArgumentCaptor.forClass(Tag.class);
-//        verify(tagRepositoryMock, times(1)).findById(tagDto.getId());
-//        verify(tagRepositoryMock, times(1)).update(argumentCaptor.capture());
-//        verifyNoMoreInteractions(tagRepositoryMock);
-//
-//        Tag tag = argumentCaptor.getValue();
-//
-//        assertTrue(tag.getId() > 0);
-//        assertThat(tag.getName(), not(tagDto.getName()));
-//    }
-//
-//    @Test
-//    public void shouldDeleteTag() {
-//        TagDto tagDto = getTestTagDto();
-//        tagDto.setId(nextLong(1, Long.MAX_VALUE));
-//        Tag tagEntity = convertToEntity(tagDto);
-//
-//        when(modelMapperMock.map(tagDto, Tag.class)).thenReturn(tagEntity);
-//        when(modelMapperMock.map(tagEntity, TagDto.class)).thenReturn(tagDto);
-//
-//        tagService.delete(tagDto);
-//
-//        verify(tagRepositoryMock, times(1)).delete(tagEntity);
-//        verifyNoMoreInteractions(tagRepositoryMock);
-//    }
+    @Test
+    public void shouldCreateTagDto() {
+        TagDto tagDto = getTestTagDto();
+        Tag tagEntity = TestUtil.convertToEntity(tagDto);
+        tagEntity.setId(nextLong(1, Long.MAX_VALUE));
+
+        when(tagRepositoryMock.save(any())).thenReturn(tagEntity);
+
+        TagDto dto = tagService.create(tagDto);
+
+
+        verify(tagRepositoryMock, times(1)).save(any());
+        verifyNoMoreInteractions(tagRepositoryMock);
+
+        assertTrue(dto.getId() > 0);
+        assertThat(dto.getName(), is(tagDto.getName()));
+    }
+
+    @Test
+    public void shouldReadTagDto() {
+        TagDto tagDto = getTestTagDto();
+        tagDto.setId(nextLong(1, Long.MAX_VALUE));
+        Tag tagEntity = convertToEntity(tagDto);
+
+        when(tagRepositoryMock.findById(anyLong())).thenReturn(Optional.of(tagEntity));
+
+        Optional<TagDto> dtoOptional = tagService.read(tagDto);
+
+        verify(tagRepositoryMock, times(1)).findById(anyLong());
+        verifyNoMoreInteractions(tagRepositoryMock);
+
+        assertTrue(dtoOptional.isPresent());
+        assertEquals(tagDto.getName(), dtoOptional.get().getName());
+    }
+
+    @Test
+    public void shouldUpdateTagName() {
+        TagDto tagDto = getTestTagDto();
+        tagDto.setId(nextLong(1, Long.MAX_VALUE));
+        Tag tagEntity = convertToEntity(tagDto);
+        tagEntity.setName(RandomStringUtils.random(10, true, false));
+
+        when(tagRepositoryMock.update(any())).thenReturn(tagEntity);
+        when(tagRepositoryMock.findById(tagDto.getId())).thenReturn(Optional.of(tagEntity));
+
+        Optional<TagDto> tagDtoOptional = tagService.update(tagDto);
+
+        verify(tagRepositoryMock, times(1)).findById(tagDto.getId());
+        verify(tagRepositoryMock, times(1)).update(any());
+        verifyNoMoreInteractions(tagRepositoryMock);
+
+        assertTrue(tagDtoOptional.isPresent());
+        assertTrue(tagDtoOptional.get().getId() > 0);
+        assertThat(tagDtoOptional.get().getName(), not(tagDto.getName()));
+    }
+
+    @Test
+    public void shouldDeleteTag() {
+        TagDto tagDto = getTestTagDto();
+        tagDto.setId(nextLong(1, Long.MAX_VALUE));
+        Tag tagEntity = convertToEntity(tagDto);
+
+        tagService.delete(tagDto);
+
+        verify(tagRepositoryMock, times(1)).delete(tagEntity);
+        verifyNoMoreInteractions(tagRepositoryMock);
+    }
 }
