19ab0e92d59b1de3c3e1fb8e33a253d4f02d9db8
Andrei_Zdanovich <Andrei_Zdanovich@epam.com>
2020-04-19 02:23:58 +0300

    add token handler and login

--- news-manager/src/App.js
+++ news-manager/src/App.js
@@ -4,32 +4,52 @@ import Footer from "./components/Footer/Footer";
 import style from './App.module.css'
 import NavBar from "./components/NavBar/NavBar";
 import NewsContainer from "./components/News/NewsContainer";
-import {Route, withRouter} from "react-router-dom";
+import {Route, withRouter, Switch} from "react-router-dom";
 import SingleNewsContainer from "./components/News/SingleNewsContainer";
 import AddNewsContainer from "./components/AddNews/AddNewsContainer";
 import EditNewsContainer from "./components/EditNews/EditNewsContainer";
 import AuthorContainer from "./components/Authors/AuthorContainer";
 import TagContainer from "./components/Tags/TagContainer";
+import Login from "./components/Login/Login";
+import {compose} from "redux";
+import {connect} from "react-redux";
+import {initializeApp} from "./redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
+
+// import {Route, Switch, withRouter} from 'react-router';
 
 class App extends Component {
 
+    componentDidMount() {
+        this.props.initializeApp();
+    }
+
     render() {
-        return (
-            <div>
-                <Header/>
-                <Footer/>
-                <div className={style.contentWrapper}>
-                    <NavBar/>
-                    <Route exact path='/news' render={() => <NewsContainer/>}/>
-                    <Route path='/news/:newsId' render={() => <SingleNewsContainer/>}/>
-                    <Route exact path='/addNews' render={() => <AddNewsContainer/>}/>
-                    <Route path='/editNews/:newsId' render={() => <EditNewsContainer/>}/>
-                    <Route path='/authors' render={() => <AuthorContainer/>}/>
-                    <Route path={'/tags'} render={() => <TagContainer/>}/>
+        if (!this.props.initialized) {
+            return <Preloader/>
+        } else {
+            return (
+                <div>
+                    <Header/>
+                    <Footer/>
+                    <div className={style.contentWrapper}>
+                        <NavBar/>
+                        <Route exact path='/news' render={() => <NewsContainer/>}/>
+                        <Route path='/news/:newsId' render={() => <SingleNewsContainer/>}/>
+                        <Route exact path='/addNews' render={() => <AddNewsContainer/>}/>
+                        <Route path='/editNews/:newsId' render={() => <EditNewsContainer/>}/>
+                        <Route path='/authors' render={() => <AuthorContainer/>}/>
+                        <Route path={'/tags'} render={() => <TagContainer/>}/>
+                        <Route path={'/login'} render={() => <Login/>}/>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
-export default withRouter(App);
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+});
+
+export default compose(withRouter, connect(mapStateToProps, {initializeApp}))(App);
--- news-manager/src/api/api.js
+++ news-manager/src/api/api.js
@@ -1,59 +1,117 @@
 import axios from "axios";
+import {tokenProvider} from "./oAuth";
+import * as Querystring from "query-string";
 
-const instance = axios.create({
-    // withCredentials: true,
-    baseURL: 'http://localhost:8080/news-manager/'
-});
+const jwtDecode = require('jwt-decode');
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost:8080/news-manager/api',
+//     headers: tokenProvider.getToken() ? {Authorization: `Bearer ${tokenProvider.getToken()}`} : null
+// });
+
+let getRequestInstance = () => {
+    let token = tokenProvider.getToken();
+    return axios.create({
+        baseURL: 'http://localhost:8080/news-manager/api',
+        headers: token ? {Authorization: `Bearer ${token}`} : null
+    });
+};
+
+export const userAPI = {
+    getUserByUserName(username) {
+        debugger;
+        return getRequestInstance().get('users?username=' + username);
+    },
+    getUserDataByToken() {
+        let token = tokenProvider.getToken();
+        let decode = token ? jwtDecode(token) : null;
+        return decode ? decode.user_name : null;
+    }
+};
+
+export const oAuthAPI = {
+    login(username, password) {
+        return axios.request({
+            url: "/oauth/token",
+            method: "POST",
+            baseURL: "http://localhost:8080/news-manager",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            auth: {
+                username: 'clientCredentials',
+                password: 'credentials'
+            },
+            data: Querystring.stringify({
+                grant_type: "password",
+                username: username,
+                password: password
+            })
+        })
+            .then(response => {
+                if (response.status === 200) {
+                    tokenProvider.setToken(response.data);
+                }
+                return response;
+            });
+    },
+    logout() {
+        tokenProvider.setToken(null);
+    },
+    isLogin() {
+        return !!tokenProvider.getToken();
+    }
+};
 
 export const newsAPI = {
     getNews(currentPage = 1, pageSize = 10, authorsName = null, tagsName = null) {
         let authorSearch = Array.isArray(authorsName) && authorsName.length ? ('&authors_name=' + authorsName) : '';
         let tagSearch = Array.isArray(tagsName) && tagsName.length ? ('&tags_name=' + tagsName) : '';
-        return instance.get(`news?count=${pageSize}&page=${currentPage}` + authorSearch + tagSearch)
+        return getRequestInstance().get(`news?count=${pageSize}&page=${currentPage}` + authorSearch + tagSearch)
             .then(response => {
                 return response.data;
             });
     },
     getNewsById(id) {
-        return instance.get('news/' + id)
+        return getRequestInstance().get('news/' + id)
             .then(response => {
                 return response.data;
             });
     },
     postNews(news) {
-        return instance.post('news', news);
+        return getRequestInstance().post('news', news);
     },
     putNews(news) {
-        return instance.put('news', news);
+        return getRequestInstance().put('news', news);
     },
     deleteNews(id) {
-        return instance.delete('news/' + id);
+        return getRequestInstance().delete('news/' + id);
     }
 };
 
 export const tagsAPI = {
     getTags() {
-        return instance.get('tags');
+        return getRequestInstance().get('tags');
     },
     postTag(tag) {
-        return instance.post('tags', tag);
+        return getRequestInstance().post('tags', tag);
     },
     put(tag) {
-        return instance.put('tags', tag);
+        return getRequestInstance().put('tags', tag);
     }
 };
 
 export const authorAPI = {
     getAuthors() {
-        return instance.get('authors')
+        return getRequestInstance().get('authors')
             .then(response => {
                 return response.data;
             });
     },
     putAuthor(author) {
-        return instance.put('authors', author);
+        return getRequestInstance().put('authors', author);
     },
     postAuthor(author) {
-        return instance.post('authors', author);
+        return getRequestInstance().post('authors', author);
     }
 };
